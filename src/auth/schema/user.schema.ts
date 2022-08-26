625b3a442457b1080e51175c38dc2143ea04673c
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-08-26 23:28:44 +0600

    isAdmin field added on user schema

--- src/auth/schema/user.schema.ts
+++ src/auth/schema/user.schema.ts
@@ -6,20 +6,22 @@ export type UserDocument = User & Document;
 
 @Schema({ timestamps: true })
 export class User {
-    @Prop({ type: SchemaTypes.String, required: [true, 'User name is required'] })
+    @Prop({ default: false })
+    isAdmin: boolean;
+
+    @Prop({ required: [true, 'User name is required'] })
     fullName: string;
 
     @Prop({
-        type: SchemaTypes.String,
         required: [true, 'User email address is required'],
         unique: true,
     })
     email: string;
 
-    @Prop({ type: SchemaTypes.String, required: [true, 'User phone number is required'] })
+    @Prop({ required: [true, 'User phone number is required'] })
     phoneNumber: string;
 
-    @Prop({ type: SchemaTypes.String, required: [true, 'User password is required'] })
+    @Prop({ required: [true, 'User password is required'] })
     password: string;
 
     @Prop({ type: SchemaTypes.String, default: null })
