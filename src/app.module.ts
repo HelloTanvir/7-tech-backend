f4615f7b8e0617c32de684ac97571c9ea63c0851
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-08-28 22:20:10 +0600

    test swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { AtGuard } from './common/guards';
@@ -15,9 +13,6 @@ import { ProductModule } from './product/product.module';
         ConfigModule.forRoot({ isGlobal: true }),
         MongooseModule.forRootAsync({
             useClass: MongooseConfigService,
-        }),
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, 'api-docs'),
         }),
         AuthModule,
         CategoryModule,
