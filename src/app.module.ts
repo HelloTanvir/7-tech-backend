e073b22e5bbe84394e3b82f7a028873356ee4df2
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-08-28 19:30:46 +0600

    created swagger-ui css as a public file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { AtGuard } from './common/guards';
@@ -13,6 +15,9 @@ import { ProductModule } from './product/product.module';
         ConfigModule.forRoot({ isGlobal: true }),
         MongooseModule.forRootAsync({
             useClass: MongooseConfigService,
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public'),
         }),
         AuthModule,
         CategoryModule,
