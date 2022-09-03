d921fc8c5ddb720905c3540d22564bf877aceb4b
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-03 23:06:07 +0600

    CORS origin set

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.enableCors();
+    app.enableCors({
+        origin: 'http://localhost:3000',
+    });
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
