e16d53b8d2607d33032e1568fbe838c6122421d6
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-13 20:06:18 +0600

    API created for getting all users and single user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from '../auth/schema';
+import { UserService } from './user.service';
 
+@ApiTags('User')
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Get all users' })
+    @ApiOkResponse({ type: [User] })
+    findAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Get('/:userId')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Get a user' })
+    @ApiOkResponse({ type: User })
+    findOne(@Param('userId') userId: string | number): Promise<User> {
+        return this.userService.findOne(userId);
+    }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../auth/schema';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
     controllers: [UserController],
     providers: [UserService],
 })
