434d9328e4f8c623606d1b4fdd2af7761a0100ac
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-13 16:23:58 +0600

    imports and providers declared in the module

--- src/banner/banner.module.ts
+++ src/banner/banner.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StorageService } from '../utils';
 import { BannerController } from './banner.controller';
 import { BannerService } from './banner.service';
+import { Banner, BannerSchema } from './schema';
 
 @Module({
+    imports: [MongooseModule.forFeature([{ name: Banner.name, schema: BannerSchema }])],
     controllers: [BannerController],
-    providers: [BannerService],
+    providers: [BannerService, StorageService],
 })
 export class BannerModule {}
