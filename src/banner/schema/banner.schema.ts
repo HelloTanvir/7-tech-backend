04539c2032527e842c2383f88d02abc2b65e08ba
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-13 16:03:44 +0600

    schema created for banner

---
+++ src/banner/schema/banner.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Document } from 'mongoose';
+
+export type BannerDocument = Banner & Document;
+
+@Schema({ timestamps: true })
+export class Banner {
+    @ApiProperty({ type: [String], isArray: true })
+    @Prop({ type: [String], required: [true, 'Banner images are required'] })
+    images: string[];
+
+    @ApiProperty({ type: [String], isArray: true })
+    @Prop({ type: [String], required: [true, 'Banner image keys are required'] })
+    keys: string[];
+}
+
+export const BannerSchema = SchemaFactory.createForClass(Banner);
---
+++ src/banner/schema/index.ts
@@ -0,0 +1,3 @@
+export * from './banner.schema';
+// eslint-disable-next-line prettier/prettier
+
