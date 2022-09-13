dc8a3726c889d1d8e841884cfe105ca0c2e03baf
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-13 16:14:06 +0600

    banner create API added

--- src/banner/banner.controller.ts
+++ src/banner/banner.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    HttpCode,
+    HttpStatus,
+    Post,
+    UnauthorizedException,
+    UploadedFiles,
+    UseFilters,
+    // eslint-disable-next-line prettier/prettier
+    UseInterceptors
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { GetCurrentUser } from '../common/decorators';
+import { HttpExceptionFilter, imageUploadOptions } from '../utils';
+import { BannerService } from './banner.service';
+import { Banner } from './schema';
 
+@ApiTags('Banner')
 @Controller('banner')
-export class BannerController {}
+export class BannerController {
+    constructor(private bannerService: BannerService) {}
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @ApiOperation({ summary: 'Add banners' })
+    @ApiCreatedResponse({ type: Banner })
+    @ApiBearerAuth()
+    @UseFilters(HttpExceptionFilter)
+    @UseInterceptors(FilesInterceptor('images', 4, imageUploadOptions))
+    create(
+        @GetCurrentUser('isAdmin') isAdmin: boolean,
+        @UploadedFiles() images: Array<Express.Multer.File>
+    ): Promise<Banner> {
+        if (!isAdmin) {
+            throw new UnauthorizedException('you are not the admin');
+        }
+
+        return this.bannerService.create(images);
+    }
+}
--- src/banner/banner.service.ts
+++ src/banner/banner.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { StorageService } from '../utils';
+import { Banner, BannerDocument } from './schema';
 
 @Injectable()
-export class BannerService {}
+export class BannerService {
+    constructor(
+        @InjectModel(Banner.name) private readonly bannerModel: Model<BannerDocument>,
+        private readonly storageService: StorageService
+    ) {}
+
+    async create(images: Express.Multer.File[]): Promise<Banner> {
+        if (!images.length) {
+            throw new ForbiddenException('Banner images are required');
+        }
+
+        // upload images
+        const imagePaths: string[] = [];
+        const keys: string[] = [];
+
+        for (const image of images) {
+            const { location, key } = await this.storageService.uploadFile(image);
+            imagePaths.push(location);
+            keys.push(key);
+        }
+
+        const banner = new this.bannerModel({
+            images: imagePaths,
+            keys,
+        });
+
+        await banner.save();
+
+        return banner;
+    }
+}
