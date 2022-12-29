d70c2bd540a285bd85ecc0ae388dc91af19004fe
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-12-29 13:31:51 +0600

    product category updating for the category update

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from '../product/schema';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { Category, CategorySchema } from './schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }])],
+    imports: [
+        MongooseModule.forFeature([
+            { name: Category.name, schema: CategorySchema },
+            { name: Product.name, schema: ProductSchema },
+        ]),
+    ],
     controllers: [CategoryController],
     providers: [CategoryService],
 })
