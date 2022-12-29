a7ca5cb43de286f485ad18313f25bc9b8f6cd8d9
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-12-29 13:30:15 +0600

    updating the main category only

--- src/category/dto/category-update.dto.ts
+++ src/category/dto/category-update.dto.ts
@@ -1,38 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform, Type } from 'class-transformer';
-import { IsArray, IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
-
-class SubCategory {
-    @ApiProperty({ example: 'Macbook', description: 'Title of the sub-category' })
-    @IsOptional()
-    @IsNotEmpty()
-    @IsString()
-    name: string;
-}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CategoryUpdateDto {
     @ApiProperty({ example: 'Laptop', description: 'Title of the category', required: false })
     @IsOptional()
     @IsNotEmpty()
     @IsString()
-    name: string;
-
-    @ApiProperty({ type: [SubCategory], required: false })
-    @IsOptional()
-    @IsNotEmpty()
-    @Transform(
-        ({ value }) => {
-            if (value && typeof value === 'string') {
-                return JSON.parse(value);
-            } else if (value && typeof value === 'object') {
-                return value;
-            }
-            return [];
-        },
-        { toClassOnly: true }
-    )
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => SubCategory)
-    subCategories: SubCategory[];
+    name?: string;
 }
