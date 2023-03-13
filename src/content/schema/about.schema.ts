bc93cc783bc28e140a10ebed7d640339efa69825
HelloTanvir <tanvirhossain.butex@gmail.com>
2023-03-13 21:49:49 +0600

    content db design

---
+++ src/content/schema/about.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Schema()
+export class About {
+    @ApiProperty()
+    @Prop({ enum: ['introduction', 'objective', 'partners & affiliates'], default: 'disclaimer' })
+    title: string;
+
+    @ApiProperty()
+    @Prop({ default: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' })
+    description: string;
+
+    @ApiProperty({ type: [String] })
+    @Prop({ type: [String], default: [] })
+    topics: string[];
+}
+
+export const AboutSchema = SchemaFactory.createForClass(About);
