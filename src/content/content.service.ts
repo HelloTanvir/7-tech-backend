fef60a43d3dd97fd388caf84f2ff179036ce0ee0
HelloTanvir <tanvirhossain.butex@gmail.com>
2023-03-13 21:50:15 +0600

    content API module setup

---
+++ src/content/content.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateContentDto } from './dto/create-content.dto';
+import { UpdateContentDto } from './dto/update-content.dto';
+import { Content, ContentDocument } from './schema';
+
+@Injectable()
+export class ContentService {
+    constructor(@InjectModel(Content.name) private readonly contentModel: Model<ContentDocument>) {}
+    create(createContentDto: CreateContentDto) {
+        return 'This action adds a new content';
+    }
+
+    findAll() {
+        return `This action returns all content`;
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} content`;
+    }
+
+    update(id: number, updateContentDto: UpdateContentDto) {
+        return `This action updates a #${id} content`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} content`;
+    }
+}
