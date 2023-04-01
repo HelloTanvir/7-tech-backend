643dc9a312843645d9d51ae948db7afacc6ee9ed
HelloTanvir <tanvirhossain.butex@gmail.com>
2023-04-01 16:00:58 +0600

    refactored content service to avoid code duplication

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -1,92 +1,130 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { TermsCreateDto, TermsUpdateDto } from './dto';
-import { Content, ContentDocument, Terms } from './schema';
+import { AboutCreateDto, PrivacyCreateDto, TermsCreateDto, TermsUpdateDto } from './dto';
+import { About, Content, ContentDocument, Privacy, Terms } from './schema';
 
 @Injectable()
 export class ContentService {
     constructor(@InjectModel(Content.name) private readonly contentModel: Model<ContentDocument>) {}
     async createTerms(termsCreateDto: TermsCreateDto): Promise<Terms> {
-        const content = await this.contentModel.findOne();
-
-        if (!content) {
-            const newContent = new this.contentModel();
-            newContent.terms.push(termsCreateDto);
-            await newContent.save();
-
-            return newContent.terms[0];
-        }
-
-        content.terms.push(termsCreateDto);
-        await content.save();
-
-        return content.terms[content.terms.length - 1];
+        const terms = await this.createContentElementItems('terms', termsCreateDto);
+        return terms;
     }
 
     async findTerms(): Promise<Content['terms']> {
-        const content = await this.contentModel.findOne();
-
-        if (!content) {
-            throw new ForbiddenException('Content not found');
-        }
-
-        return content.terms;
+        const terms = await this.findContentElement('terms');
+        return terms;
     }
 
     async findTerm(termsId: string): Promise<Terms> {
-        const content = await this.contentModel.findOne();
-
-        if (!content) {
-            throw new ForbiddenException('Content not found');
-        }
-
-        const term = content.terms.find((t) => (t as any)._id == termsId);
-
-        if (!term) {
-            throw new ForbiddenException('Term not found');
-        }
-
+        const term = await this.findContentElementItems('terms', termsId);
         return term;
     }
 
     async updateTerm(termsId: string, termsUpdateDto: TermsUpdateDto): Promise<Terms> {
-        const content = await this.contentModel.findOne();
-
-        if (!content) {
-            throw new ForbiddenException('Content not found');
-        }
-
-        const term = content.terms.find((t) => (t as any)._id == termsId);
-
-        if (!term) {
-            throw new ForbiddenException('Term not found');
-        }
-
-        const index = content.terms.indexOf(term);
-        content.terms[index] = { ...term, ...termsUpdateDto };
-        await content.save();
-
-        return content.terms[index];
+        const term = await this.updateContentElementItems('terms', termsId, termsUpdateDto);
+        return term;
     }
 
     async deleteTerm(termsId: string): Promise<Terms> {
+        const term = await this.deleteContentElementItems('terms', termsId);
+        return term;
+    }
+
+    async findContentElement(
+        type: 'terms' | 'privacy' | 'about'
+    ): Promise<Content['terms'] | Content['privacy'] | Content['about']> {
         const content = await this.contentModel.findOne();
 
         if (!content) {
             throw new ForbiddenException('Content not found');
         }
 
-        const term = content.terms.find((t) => (t as any)._id == termsId);
+        return content[type];
+    }
 
-        if (!term) {
-            throw new ForbiddenException('Term not found');
+    async findContentElementItems(
+        type: 'terms' | 'privacy' | 'about',
+        itemId: string
+    ): Promise<Terms | Privacy | About> {
+        const content = await this.contentModel.findOne();
+
+        if (!content) {
+            throw new ForbiddenException('Content not found');
         }
 
-        const index = content.terms.indexOf(term);
-        content.terms.splice(index, 1);
+        const item = content[type].find((t) => (t as any)._id == itemId);
+
+        if (!item) {
+            throw new ForbiddenException(`${type} with ${itemId} not found`);
+        }
+
+        return item;
+    }
+
+    async createContentElementItems(
+        type: 'terms' | 'privacy' | 'about',
+        dto: TermsCreateDto | PrivacyCreateDto | AboutCreateDto
+    ): Promise<Terms | Privacy | About> {
+        const content = await this.contentModel.findOne();
+
+        if (!content) {
+            const newContent = new this.contentModel();
+            newContent[type].push(dto);
+            await newContent.save();
+            return newContent[type][0];
+        }
+
+        content[type].push(dto);
+        await content.save();
+        return content[type][content[type].length - 1];
+    }
+
+    async updateContentElementItems(
+        type: 'terms' | 'privacy' | 'about',
+        itemId: string,
+        dto: TermsCreateDto | PrivacyCreateDto | AboutCreateDto
+    ): Promise<Terms | Privacy | About> {
+        const content = await this.contentModel.findOne();
+
+        if (!content) {
+            throw new ForbiddenException('Content not found');
+        }
+
+        const item = content[type].find((t) => (t as any)._id == itemId);
+
+        if (!item) {
+            throw new ForbiddenException(`${type} with ${itemId} not found`);
+        }
+
+        const index = content[type].indexOf(item);
+        content[type][index] = { ...item, ...dto };
         await content.save();
 
-        return term;
+        return content[type][index];
+    }
+
+    async deleteContentElementItems(
+        type: 'terms' | 'privacy' | 'about',
+        itemId: string
+    ): Promise<Terms | Privacy | About> {
+        const content = await this.contentModel.findOne();
+
+        if (!content) {
+            throw new ForbiddenException('Content not found');
+        }
+
+        const item = content[type].find((t) => (t as any)._id == itemId);
+
+        if (!item) {
+            throw new ForbiddenException(`${type} with ${itemId} not found`);
+        }
+
+        const index = content[type].indexOf(item);
+        content[type].splice(index, 1);
+        await content.save();
+
+        return item;
     }
 }
