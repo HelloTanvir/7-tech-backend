473e5a8ca5a9d4d448b8782240f9fe8a22207c5f
HelloTanvir <tanvirhossain.butex@gmail.com>
2023-09-01 00:09:10 +0600

    updated order status validation

--- src/order/dto/order-update.dto.ts
+++ src/order/dto/order-update.dto.ts
@@ -5,10 +5,32 @@ export class OrderUpdateDto {
     @ApiProperty({
         example: 'delivered',
         description: 'Status of the order',
-        enum: ['pending', 'delivered', 'cancelled'],
+        enum: [
+            'pending',
+            'delivered',
+            'cancelled',
+            'confirmed',
+            'unreachable',
+            'hold',
+            'paid',
+            'processing',
+            'shiped',
+            'refunded',
+        ],
     })
     @IsNotEmpty()
     @IsString()
-    @IsIn(['pending', 'delivered', 'cancelled'])
+    @IsIn([
+        'pending',
+        'delivered',
+        'cancelled',
+        'confirmed',
+        'unreachable',
+        'hold',
+        'paid',
+        'processing',
+        'shiped',
+        'refunded',
+    ])
     status: string;
 }
